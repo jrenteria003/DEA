32a3bf95c546b53d975f3cecd1db508cb1527db5
jonr <jrenteria003@ikasle.ehu.eus>
2020-01-10 18:46:08 +0100

    3.ariketa (zuhaitz genealogiko faxista) amaitu

--- azterketak/2018_2019/3.ariketa.java
+++ azterketak/2018_2019/3.ariketa.java
@@ -13,7 +13,39 @@ public class ZuhaitzGenealogikoa {
 
 	public boolean bizkaitarPetoPetoaDa(String izena,
 			HashMap<String,String> herriak) {
-		//
-		
+		BinaryTreeNode<Pertsona> adabg =
+			unekoaAdabegia(izena, this.root);
+		return bizkaitarPetoPetoaDa(adabg, herriak);
+	}
+
+	private boolean bizkaitarPetoPetoaDa(BinaryTreeNode<Pertsona>
+			adabg, HashMap<String,String> herriak, boolean egoera) {
+		if(!egoera) return false;
+		else if(adabg == null) return true;
+		else {
+			egoera = bizkaitarraDa(adabg.data);
+			boolean ezker = bizkaitarPetoPetoaDa(adabg.left, herriak, egoera);
+			boolean eskum = bizkaitarPetoPetoaDa(adabg.right, herriak, egoera);
+			return ezker && eskum;
+		}
+	}
+
+	private BinaryTreeNode<T> aurkituAdabegia(String pIzena,
+			BinaryTreeNode<T> unekoaAdabegia) {
+		if(unekoaAdabegia == null) return null;
+		else if(unekoaAdabegia.data.izena.equals(pIzena)) {
+			return unekoaAdabegia;
+		} else {
+			BinaryTreeNode<T> emaitza = aurkituAdabegia(pIzena,
+					unekoaAdabegia.left);
+			if(emaitza != null) return emaitza;
+			return aurkituAdabegia(pIzena,
+					unekoaAdabegia.right);
+		}
+	}
+
+	private boolean bizkaitarraDa(Pertsona p, 
+			HashMap<String,String> herriak) {
+		return herriak.get(p.jaioterria)=="Bizkaia";
 	}
 }
