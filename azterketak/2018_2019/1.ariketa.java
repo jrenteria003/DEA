6174c4460a44cf4a262b528afc2b946ebedef7d7
jonr <jrenteria003@ikasle.ehu.eus>
2020-01-10 19:08:15 +0100

    ariketa 'hasi'

---
+++ azterketak/2018_2019/1.ariketa.java
@@ -0,0 +1,17 @@
+public class Pertsona {
+	String id;
+	Pertsona[] lagunak;
+}
+
+public class Adabegi {
+	Pertsona info;
+	Adabegi next;
+}
+
+public class Lista {
+	Adabegi first;
+
+	public void gehitu(String id, String[] kideak) {
+
+	}
+}
