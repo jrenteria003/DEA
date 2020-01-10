403218aefad2fe3dd59849ca200f3c85275930cd
jonr <jrenteria003@ikasle.ehu.eus>
2020-01-10 18:46:41 +0100

    4.ariketa.java sortu

---
+++ azterketak/2018_2019/4.ariketa.java
@@ -0,0 +1,11 @@
+public class KontaktuSarea {
+	private boolean[][] adjMatrix;
+
+	public int lagungarriena(ArrayList<Integer> pertsonak) {
+
+	}
+
+	private boolean bateragarriena(Integer p1, Integer p2) {
+		return true;
+	}
+}
